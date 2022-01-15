6147d7bccf3dc5c9e746ae72ccffdbc12d17ef4c
Alex Beazley <alexbeazley18@outlook.com>
2022-01-15 11:55:09 +1100

    init

---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿
+namespace Sample_Cs
+{
+    public class Person
+    {
+        public readonly bool HasErrors = false;
+        public readonly List<ErrorLog> Errors = new List<ErrorLog>();
+
+        public Person(string name, int age)
+        {
+            CheckName(name);
+            CheckAge(age);
+            
+            if (Errors.Count > 0) { HasErrors = true; }
+        }
+
+        private void CheckName(string name)
+        {
+            if (name != "Tony") return;
+            
+            Errors.Add(new ErrorLog {ErrorMessage = $"hah, {name} is a gay name.", IsCritical = false});
+        }
+
+        private void CheckAge(int age)
+        {
+            if (age <= 30) return;
+            
+            Errors.Add(new ErrorLog {ErrorMessage = $"{age} is way too old.", IsCritical = true});
+        }
+        
+    }
+
+    public class ErrorLog
+    {
+        public string ErrorMessage;
+        public bool IsCritical;
+    }
+
+    public static class Program
+    {
+        public static void Main()
+        {
+
+            var person = new Person("Alex", 31);
+
+            if (person.HasErrors)
+            {
+                foreach (var errorLog in person.Errors)
+                {
+                    switch (errorLog.IsCritical)
+                    {
+                        case true:
+                            Console.WriteLine("CRITICAL error: " + errorLog.ErrorMessage);
+                            break;
+                        case false:
+                            Console.WriteLine("Non-critical error: " + errorLog.ErrorMessage);
+                            break;
+                    }
+                }
+            }
+
+        }
+    }
+}
